3ba3195acf31951e2fcf35d304a3f01e0beb0f75
katsukirntaro <katsukirntaro@localhost>
2023-05-31 10:35:18 +0900

    part5

--- src/main/java/com/example/demo/web/admin/OrderController.java
+++ src/main/java/com/example/demo/web/admin/OrderController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -66,9 +67,14 @@ public class OrderController {
 	 * 受注表示
 	 */
 	@GetMapping(value = "/view/{id}")
-	public String view(Model model) {
-		List<Order> orderr = orderService.findAll();
-		model.addAttribute("orders", orderr);
+	public String view(@PathVariable Integer id, Model model, RedirectAttributes ra) {
+		FlashData flash;
+		try {
+			Order order = orderService.findById(id);
+			model.addAttribute("order", order);
+		} catch (Exception e) {
+			flash  = new FlashData().danger("処理中にエラーが発生しました");
+		}
 		return "admin/orders/view";
 	}
 	
